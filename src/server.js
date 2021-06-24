ce68e3db67e10870d6bc27622980c907025be71f
Lucas Passeri <lucaspasseri@poli.ufrj.br>
2021-06-23 22:45:34 -0300

    POST signup done.

--- src/server.js
+++ src/server.js
@@ -2,7 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import pg from 'pg';
 import bcrypt from 'bcrypt';
+import { v4 as uuid } from 'uuid';
 
+import {newUserSchema} from './schemas/signupSchema.js';
 
 const app = express();
 
@@ -20,43 +22,108 @@ const databaseConfig = {
 const { Pool } = pg;
 const connection = new Pool(databaseConfig);
 
-/* app.post("/sign-in", async (req, res) => {
-    const { email, senha } = req.body;
-    console.log(email, senha);
-    // Busque o usuário no banco e valide a senha usando bcrypt
+app.get("/validsession", async (req, res) => {
+    try {
 
-    const result = await connection.query(`SELECT * FROM users WHERE email = $1`, [email]);
-    
-    const user  = result.rows;
+        const sessions = await connection.query(
+            `SELECT * FROM sessions`
+        );
 
-    console.log(user[2]);
+        console.log(sessions.rows);
 
-    if(user && bcrypt.compareSync(senha, user[2].senha)){
-        res.sendStatus(200);
-    } else {
-        res.sendStatus(401);
+        const authorization = req.header("Authorization");
+        console.log(authorization);
+        const token = authorization?.replace("Bearer ", "");
+        console.log(token);
+
+        if(!token) return res.sendStatus(401);
+        
+        const result = await connection.query(
+            `SELECT * FROM sessions
+            JOIN users ON sessions."userId" = users.id
+            WHERE sessions.token = $1`, [token]
+        );
+        
+        const user = result.rows[0];
+        console.log(user);
+
+        if(user){
+            res.sendStatus(200);
+        } else {
+            res.sendStatus(401);
+        }
+
+    } catch(e) {
+        console.log(e);
+        res.sendStatus(500);
     }
-}); */
-app.post("/login")
+});
+
+app.post("/signin", async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        console.log(email, password);
+
+        const user = await connection.query(
+            `SELECT * 
+            FROM users 
+            WHERE email = $1`,
+            [email]
+        );
+        
+        console.log(user.rows[0]);
+        console.log(user.rows[0].hash);
+        console.log(bcrypt.compareSync(""+password, user.rows[0].hash));
+
+        if(user && bcrypt.compareSync(""+password, user.rows[0].hash)){
+            const token = uuid();
+
+            await connection.query(
+                `INSERT INTO sessions ("userId", token)
+                VALUES ($1, $2)`, [user.rows[0].id, token]
+            );
+
+            res.send(token);
+        }
+        else {
+            res.sendStatus(401);
+        }
+        
+    }catch(e){
+        console.log(e);
+        res.sendStatus(500);
+    }
+});
 
 
 app.post("/signup", async (req, res)=> {
     try {
-        const { name, email, password } = req.body;
-        console.log(name, email, password);
+        const { name, email, password, rePassword } = req.body;
+
+        const errors = newUserSchema.validate(req.body).error;
+
+        if(errors) return res.sendStatus(400);
 
         const hash = bcrypt.hashSync(password, 12);
 
-        console.log(hash);
-
-        await connection.query(`
-            INSERT INTO users 
-            (name, email, hash)
-            VALUES ($1, $2, $3)`,
-            [name, email,hash]
+        const result = await connection.query(
+            `SELECT * FROM users
+            WHERE email = $1`,
+            [email]
         );
 
-        res.sendStatus(201);
+        if(result.rows.length === 0){
+            await connection.query(`
+                INSERT INTO users 
+                (name, email, hash)
+                VALUES ($1, $2, $3)`,
+                [name, email, hash]
+            );  
+
+            res.sendStatus(201);
+        } else {
+            res.sendStatus(409);
+        }
         
     } catch(e){
         console.log(e);
@@ -69,7 +136,7 @@ app.get("/signup", async (req, res) => {
         const users = await connection.query(
             `SELECT * FROM users`
             );
-        
+            
         res.send(users.rows);
     } catch(e){
         console.log(e);
@@ -77,8 +144,11 @@ app.get("/signup", async (req, res) => {
     }
 });
 
-app.get("/", (req, res) => { 
-    res.send("oi");
+app.post("/", (req, res) => {
+    const { name, email, password, rePassword } = req.body; 
+    console.log(req.body);
+    console.log(name);
+    res.send(req.body);
 });
 
 app.listen(4000, () => {
