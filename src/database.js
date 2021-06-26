3d8d564c49bebb7bb56c2cbb1cb1f3746ae9e479
Lucas Passeri <lucaspasseri@poli.ufrj.br>
2021-06-25 22:10:59 -0300

    Prepare for deploy.

--- src/database.js
+++ src/database.js
@@ -1,14 +1,14 @@
 import pg from 'pg';
 
-const databaseConfig = {
-    user: 'postgres',
-    password: '123456',
-    database: 'my_wallet',
-    host: 'localhost',
-    port: 5432
-};
-
 const { Pool } = pg;
+
+const databaseConfig = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+}
+
 const connection = new Pool(databaseConfig);
 
 export default connection;
