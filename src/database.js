99f6e57e9b4556cab4183b55b6ea92acc7d8bd1f
Lucas Passeri <lucaspasseri@poli.ufrj.br>
2021-08-14 01:58:50 -0300

    Fixing deploy.

--- src/database.js
+++ src/database.js
@@ -2,11 +2,7 @@
 import pg from "pg";
 
 const databaseConfig = {
-	user: process.env.DB_USER,
-	password: process.env.DB_PASSWORD,
-	database: process.env.DB_DATABASE,
-	host: process.env.DB_HOST,
-	port: process.env.DB_PORT,
+	connectionString: process.env.DATABASE_URL,
 	ssl: {
 		rejectUnauthorized: false
 	}
